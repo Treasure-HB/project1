52d080854385922689c92bb8f77b8f30f8c612f5
phb1533@naver.com <phb1533@naver.com>
2023-04-04 11:28:56 +0900

    수정

--- hello.java
+++ hello.java
@@ -1,6 +1,8 @@
 public class HelloWorld{
     public static void main(String[] args){
        
+       //hello
+       System.out.println("hello World!")
         System.out.println("Treasure love it");
          System.out.println("Hello World!");
           
